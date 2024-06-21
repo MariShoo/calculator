60fcfa2187d9625ef763467e95235a7f76795393
Giorgi Khvichia <116484981+GioGio2004@users.noreply.github.com>
2024-06-21 18:02:16 +0400

    calculator_two.py

---
+++ calculator_two.py
@@ -0,0 +1,34 @@
+
+op_type = input ("choose an operation to use")
+
+a = int(input("enter first number :"))
+b = int(input("enter second number :"))
+
+run = True
+
+def add(a, b):
+    print( a + b) 
+
+def sub(a, b):
+    print( a - b) 
+
+def multiply(a, b):
+    print( a * b)
+
+def divide(a, b):
+    print( a / b)
+
+while run :
+
+    if "+" in op_type:
+        add(a , b)
+        break
+    if "-" in op_type:
+        sub(a , b)
+        break
+    if "*" in op_type:
+        multiply(a , b)
+        break
+    if "/" in op_type:
+        divide(a , b)
+        break
